0f522fea1c96ccacf1b45247b3d739e60c359b10
SABBIDO7 <68105510+SABBIDO7@users.noreply.github.com>
2024-06-02 20:05:25 +0300

    testWeather (#2)

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ public class WeatherForecastController : ControllerBase
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
+    //test
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
