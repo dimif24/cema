9022bd05d134619f63e4ce51e2e64f3bcca4e47c
dimif24 <131251592+dimif24@users.noreply.github.com>
2024-06-02 19:16:29 +0200

    feat(CEMA-0): Demo Git Workflow (#1)
    * changed title
    
    * added paragraph in app component
    
    * added will
    
    ---------
    
    Co-authored-by: Dimitri Flouty <dimif24@hotmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,10 @@ function App() {
   return (
     <div>
       <p className="text-xl bg-red-500 font-bold underline">
-      CEMA
+      CEMA Project Started
+      </p>
+      <p>
+      Hello World will
       </p>
     </div>
   )
